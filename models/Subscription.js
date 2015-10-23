9b717019ffdaa4afdc56e82e4bbe5b86cff15dd7
jkc621 <jameskitamirike@gmail.com>
2015-10-23 22:21:45 +0200

    Add subscription model

---
+++ models/Subscription.js
@@ -0,0 +1,23 @@
+var Subscription = sequelize.define('Subscription', {
+    user_id: {
+        allowNull: false,
+        primaryKey: true,
+        references: {
+            model: Profile,
+            key: "user_id"
+        },
+        type: Sequelize.STRING(15)
+    }
+    site_id: {
+        allowNull: false,,
+        primaryKey: true,
+        references: {
+            model: Site,
+            key: "site_id"
+        },
+        type: Sequelize.UUID
+    }
+}, {
+    timestamps: true,
+    underscored: true
+})
