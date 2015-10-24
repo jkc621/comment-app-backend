b8834cc001e4ced3413e062d0fd3d3bb1b9d378b
jkc621 <jameskitamirike@gmail.com>
2015-10-24 08:57:18 +0200

    Add Page Model

---
+++ models/Page.js
@@ -0,0 +1,18 @@
+var Page = sequelize.Define('Page', {
+    url: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+        validate: {
+            isUrl: true
+        }
+    },
+    site_id: {
+        allowNull: false,
+        references: {
+            model: Site,
+            key: "site_id"
+        },
+        type: Sequelize.UUID
+    }
+}, {})
